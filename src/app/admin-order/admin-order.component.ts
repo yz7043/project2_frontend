7d61e10cd71f0cedbd32e84f4247a12f435f3dae
yz7043 <90709617+yz7043@users.noreply.github.com>
2023-07-04 00:15:24 -0400

    1. admin order page

--- src/app/admin-order/admin-order.component.ts
+++ src/app/admin-order/admin-order.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RestApiService} from "../services/rest-api.service";
+import {AdminAllOrderResponse} from "../models/admin-order-dto";
+import {AdminOrderDetailResponse} from "../models/order-item-detail-dto";
+import {catchError} from "rxjs";
 
 @Component({
   selector: 'app-admin-order',
   templateUrl: './admin-order.component.html',
   styleUrls: ['./admin-order.component.css']
 })
-export class AdminOrderComponent {
+export class AdminOrderComponent implements OnInit{
+  orders: AdminAllOrderResponse | null = null;
+  isShowDetail: boolean = false;
+  curOrder: AdminOrderDetailResponse | null = null;
+  constructor(private restAPI: RestApiService) {
+  }
 
+  ngOnInit(): void {
+    this.refreshData(0);
+  }
+
+  refreshData(page = 0){
+    this.refreshAllOrders(page);
+  }
+
+  refreshAllOrders(page = 0){
+    this.restAPI.getAdminOrders(page)
+      .pipe(catchError(this.restAPI.handleError))
+      .subscribe({
+        next: (resp) => {
+          this.orders = resp;
+        },
+        error: (err) => {
+          alert(err.error.message);
+        }
+      });
+  }
+  detailOpen(id: number){
+    this.restAPI.getAdminOrderDetail(id)
+      .pipe(catchError(this.restAPI.handleError))
+      .subscribe({
+        next: (resp) =>{
+          this.isShowDetail = true;
+          this.curOrder = resp;
+          console.log(resp)
+        },
+        error: (err) => {
+          alert(err.error.message)
+        }
+      })
+  }
+  detailClose() {
+    this.curOrder = null;
+    this.isShowDetail = false;
+  }
+
+  nextPage(){
+    if(this.orders && this.orders.currentPage + 1 < this.orders.totalPages)
+      this.refreshData(this.orders.currentPage + 1)
+  }
+  prevPage(){
+    if(this.orders && this.orders.currentPage > 0){
+      this.refreshData(this.orders.currentPage - 1);
+    }
+  }
+  cancelOrder(id: number){
+    this.restAPI.adminCancelOrder(id)
+      .pipe(catchError(this.restAPI.handleError))
+      .subscribe({
+        next: (resp) => {
+          alert(`Order ${id} cancelled!`);
+          this.refreshData(this.orders ? this.orders.currentPage : 0);
+        },
+        error: (err) => {
+          alert(err.error.message);
+        }
+      })
+  }
+
+  completeOrder(id: number){
+    this.restAPI.adminCompleteOrder(id)
+      .pipe(catchError(this.restAPI.handleError))
+      .subscribe({
+        next: (resp) => {
+          alert(`Order ${id} completed!`);
+          this.refreshData(this.orders ? this.orders.currentPage : 0);
+        },
+        error: (err) => {
+          alert(err.error.message);
+        }
+      })
+  }
 }
